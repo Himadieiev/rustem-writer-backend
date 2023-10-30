976352474299eb80885cce93f0481b9812330436
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-30 16:24:09 +0200

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import connectDB from "./config/db.js";
 
@@ -16,6 +17,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
